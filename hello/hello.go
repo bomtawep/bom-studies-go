6e48d29920a596873500fa6a8454d0a96aea3f9b
Taweewat Phimhin <tawep462@orion.postbox.in.th>
2023-04-02 13:53:12 +0700

    add readme and set env

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bom-studies-go/greetings"
+	"bom-studies-go/stringutil"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println(greetings.Hello("bom"))
+	fmt.Println(stringutil.ToUpper("bom to upper"))
 }
